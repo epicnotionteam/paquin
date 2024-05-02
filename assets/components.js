86de303df5dc7e22bc8eefa49f5d8e4d6c56da9b
Graeme Bryson <graemebryson5@gmail.com>
2024-05-01 22:07:17 -0400

    Testing custom grid structure

--- assets/components.js
+++ assets/components.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var iso = new Isotope('.grid--product', {
-        itemSelector: '.grid__item',
-        masonry: {
-            columnWidth: 250
-        }
+    const gridItems = document.querySelectorAll('.grid__item');
+  
+    const resizeGridItem = item => {
+      const grid = document.getElementsByClassName("grid--product")[0];
+      const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+      const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+      const rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+      item.style.gridRowEnd = "span " + rowSpan;
+    };
+  
+    gridItems.forEach(item => {
+      resizeGridItem(item);
+      item.addEventListener('load', () => {
+        resizeGridItem(item);
+      });
     });
-});
+  
+    window.addEventListener('resize', () => {
+      gridItems.forEach(item => {
+        resizeGridItem(item);
+      });
+    });
+  });
+  
